c64fd8e0621ac3bbc541f5e9d30e050992b0dfd2
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-20 18:00:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,16 +9,4 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 
-  class Dog
-    attr_accessor: name, breed, age
-
-    def initialize(name, breed, age)
-      @name = name
-      @breed = breed
-      @age = age
-    end
-
-
-
-  end
 end
